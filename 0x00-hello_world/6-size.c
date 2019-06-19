ec59a82fb52dfcfa1ec13b8038ec08639d65b856
SantiagoHerreG <888@holbertonschool.com>
2019-06-19 20:59:10 +0000

    Print size of type of data using sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - print the size of diferent types
+ * ch: saves the byte value of a char
+ * i: saves the byte value of a int
+ * l: saves the byte value of a long
+ * llo: saves the byte value of a long long int
+ * f: saves the byte value of a float
+ *
+ * Description: print a phrase with correspondent value
+ * Return: zero
+ */
+int main(void)
+{
+char ch;
+int i;
+long l;
+long long int llo;
+float f;
+printf("Size of a char: %lu byte(s)", (unsigned long)sizeof(ch));
+printf("Size of a int: %lu byte(s)", (unsigned long)sizeof(i));
+printf("Size of a long: %lu byte(s)", (unsigned long)sizeof(l));
+printf("Size of a long long int: %lu byte(s)", (unsigned long)sizeof(llo));
+printf("Size of a float: %lu byte(s)", (unsigned long)sizeof(f));
+return (0);
+}
