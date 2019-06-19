1df09072d38c6e7065846bc970152bc2bcbbc6d6
SantiagoHerreG <888@holbertonschool.com>
2019-06-19 21:09:38 +0000

    printing data type size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,7 +18,7 @@ long l;
 long long int llo;
 float f;
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(ch));
-printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 printf("Size of a long: %lu byte(s)\n", (unsigned long)sizeof(l));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(llo));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
