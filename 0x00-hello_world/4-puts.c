319bafca806d70bbcf8228afc515e1e9ce168bf8
SantiagoHerreG <888@holbertonschool.com>
2019-06-19 19:42:48 +0000

    Header was missing printing with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-main(void)
+/**
+ * main - Print a string using puts
+ *
+ * Description: print a phrase
+ * Return: zero
+ */
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
