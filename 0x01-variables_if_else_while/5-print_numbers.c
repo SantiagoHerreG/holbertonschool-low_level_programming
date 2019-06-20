324c80df7cd9ac685b28f46ca23fd523816a5df8
SantiagoHerreG <888@holbertonschool.com>
2019-06-20 20:27:42 +0000

    Program to print numbers 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print the last digit of random characters
+ *
+ * Description: print a digit
+ * Return: zero
+ */
+int main(void)
+{
+
+	int c;
+
+	for (c = '0'; c <= '9'; ++c)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
