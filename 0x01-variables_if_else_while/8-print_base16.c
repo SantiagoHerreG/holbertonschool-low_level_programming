0932d7bed16607aeac94afabd69326cfa97455ca
SantiagoHerreG <888@holbertonschool.com>
2019-06-20 20:56:33 +0000

    Printing the first numbers in hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print the last digit of random characters
+ *
+ * Description: print a digit
+ * Return: zero
+ */
+int main(void)
+{
+
+	int c;
+
+	for (c = 0; c <= 15; ++c)
+		{
+		printf("%x", c);
+		}
+	printf("\n");
+	return (0);
+}
