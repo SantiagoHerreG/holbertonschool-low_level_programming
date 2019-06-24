77c769e039962e758cce12063018c97b4f2a43e3
SantiagoHerreG <888@holbertonschool.com>
2019-06-24 18:43:39 +0000

    Folder of the project about nested loops and functions with readme and header files

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+#Readme file for the  project about nested loops and functions in C#Readme file for the  project about nested loops and functions in C
---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,10 @@
+#ifndef _holberton_h_
+#define _holberton_h_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+#endif /* _holberton_h_ */
