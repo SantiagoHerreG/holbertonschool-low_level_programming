6130c638b471999070cca8d69551248a608439b9
SantiagoHerreG <888@holbertonschool.com>
2019-06-25 01:18:44 +0000

    Function that prints the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * print_last_digit - prints last digit of a number
+ * @n: number
+ * Return: 1 if positive, 0 if zero and -1 if negative
+ */
+int print_last_digit(int n)
+{
+	int a, b;
+
+	if (n >= 0)
+	{
+	a = n % 10;
+	_putchar('0' + a);
+	return (a);
+	}
+	else
+	{
+	n = n * (-1);
+	b = n % 10;
+	_putchar('0' + b);
+	return (b);
+	}
+}
+
