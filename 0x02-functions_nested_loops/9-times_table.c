794e495bad61eca211f0d6de85df661ffcf242f3
SantiagoHerreG <888@holbertonschool.com>
2019-06-25 16:16:38 +0000

    Function that prints the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,95 +6,42 @@
 
 void times_table(void)
 {
-	char a[] = "0,  0,  0,  0,  0,  0,  0,  0,  0,  0";
+	int i = 0, a = 0;
 
-	char b[] = "0,  1,  2,  3,  4,  5,  6,  7,  8,  9";
-
-	char c[] = "0,  2,  4,  6,  8, 10, 12, 14, 16, 18";
-
-	char d[] = "0,  3,  6,  9, 12, 15, 18, 21, 24, 27";
-
-	char e[] = "0,  4,  8, 12, 16, 20, 24, 28, 32, 36";
-
-	char f[] = "0,  5, 10, 15, 20, 25, 30, 35, 40, 45";
-
-	char g[] = "0,  6, 12, 18, 24, 30, 36, 42, 48, 54";
-
-	char h[] = "0,  7, 14, 21, 28, 35, 42, 49, 56, 63";
-
-	char j[] = "0,  8, 16, 24, 32, 40, 48, 56, 64, 72";
-
-	char k[] = "0,  9, 18, 27, 36, 45, 54, 63, 72, 81";
-
-	int i = 0;
-
-	while (i < 37)
+	while (i < 10)
 	{
-	_putchar(a[i]);
+	_putchar('0');
+	_putchar(',');
+	for (a = 1; a <= 8; a++)
+	{
+	if ((a * i) < 10)
+	{
+	_putchar(' ');
+	_putchar(' ');
+	_putchar('0' + (a * i));
+	_putchar(',');
+	}
+	if ((a * i) >= 10)
+	{
+	_putchar(' ');
+	_putchar('0' + ((a * i) / 10));
+	_putchar('0' + ((a * i) % 10));
+	_putchar(',');
+	}
+	}
+	_putchar(' ');
+	if ((9 * i) < 10)
+	{
+	_putchar(' ');
+	_putchar('0' + (9 * i));
+	_putchar('\n');
+	}
+	else
+	{
+	_putchar('0' + ((9 * i) / 10));
+	_putchar('0' + ((9 * i) % 10));
+	_putchar('\n');
+	}
 	i++;
 	}
-	_putchar('\n');
-	i = 0;
-	while (i < 37)
-	{
-	_putchar(b[i]);
-	i++;
-	}
-	_putchar('\n');
-	i = 0;
-	while (i < 37)
-	{
-	_putchar(c[i]);
-	i++;
-	}
-	_putchar('\n');
-	i = 0;
-	while (i < 37)
-	{
-	_putchar(d[i]);
-	i++;
-	}
-	_putchar('\n');
-	i = 0;
-	while (i < 37)
-	{
-	_putchar(e[i]);
-	i++;
-	}
-	_putchar('\n');
-	i = 0;
-	while (i < 37)
-	{
-	_putchar(f[i]);
-	i++;
-	}
-	_putchar('\n');
-	i = 0;
-	while (i < 37)
-	{
-	_putchar(g[i]);
-	i++;
-	}
-	_putchar('\n');
-	i = 0;
-	while (i < 37)
-	{
-	_putchar(h[i]);
-	i++;
-	}
-	_putchar('\n');
-	i = 0;
-	while (i < 37)
-	{
-	_putchar(j[i]);
-	i++;
-	}
-	_putchar('\n');
-	i = 0;
-	while (i < 37)
-	{
-	_putchar(k[i]);
-	i++;
-	}
-	_putchar('\n');
 }
