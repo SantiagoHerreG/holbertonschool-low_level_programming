38317049d42a833eac45ca8f81c5a6b997882a65
SantiagoHerreG <888@holbertonschool.com>
2019-06-25 21:04:16 +0000

    Printing the n times table using nested while and for

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+/**
+ * print_times_table - prints times table until n from 0
+ * @n: number less than 16
+ * Return: nothing
+ */
+
+void print_times_table(int n)
+{
+	if (n > 15)
+	return;
+	else
+	{
+	int i = 0, a = 0;
+
+	while (i <= n)
+	{
+	_putchar('0');
+	_putchar(',');
+	for (a = 1; a <= n; a++)
+	{
+	if ((a * i) < 10)
+	{
+	_putchar(' ');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar('0' + (a * i));
+	}
+	if (((a * i) >= 10) && ((a * i) <= 99))
+	{
+	_putchar(' ');
+	_putchar(' ');
+	_putchar('0' + ((a * i) / 10));
+	_putchar('0' + ((a * i) % 10));
+	}
+	if ((a * i) > 99)
+	{
+	_putchar(' ');
+	_putchar('0' + ((a * i) / 100));
+	_putchar('0' + (((a * i) / 10) % 10));
+	_putchar('0' + ((a * i) % 10));
+	}
+	if (a != n)
+	_putchar(',');
+	}
+	i++;
+	_putchar('\n');
+	}
+	}
+}
