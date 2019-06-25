2f0c7877e056ecbf68ff0ce15cd74dbd8d3be621
SantiagoHerreG <888@holbertonschool.com>
2019-06-25 04:16:43 +0000

    The function that adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * add - prints the sum of two numbers
+ * @a: factor
+ * @b: factor
+ * Return: total
+ */
+
+int add(int a, int b)
+{
+	int c = a + b;
+
+	return (c);
+}
