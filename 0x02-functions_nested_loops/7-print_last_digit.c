27fe63ccab7b5aeb560b7bf11e389dd9d5486b50
SantiagoHerreG <888@holbertonschool.com>
2019-06-25 16:50:51 +0000

    Print the last digit of a function correction3

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,18 +2,17 @@
 /**
  * print_last_digit - prints last digit of a number
  * @n: number
+ *
  * Return: the last digit
  */
 int print_last_digit(int n)
 {
-	int a;
-
 	if (n < 0)
 	{
-	a = -n;
-	a = a % 10;
-	_putchar('0' + a);
-	return (a);
+	n = -n;
+	n = n % 10;
+	_putchar('0' + n);
+	return (n);
 	}
 	else
 	{
