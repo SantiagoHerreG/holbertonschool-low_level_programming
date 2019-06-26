0bdc1917c5907ea4e16764565420c08bd497ad03
SantiagoHerreG <888@holbertonschool.com>
2019-06-26 02:41:50 +0000

    Fibonacci numbers lower than 4000000 added corrected

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,16 +7,16 @@
 
 int main(void)
 {
-	long n, new, prev = 1, ant = 1;
+	int n, new, prev = 1, ant = 1;
 
 	while (prev < 4000000)
 	{
 	new = prev + ant;
 	ant = prev;
 	prev = new;
-	if ((ant % 2) == 0)
-	n = n + ant;
+	if ((prev % 2) == 0)
+	n = n + prev;
 	}
-	printf("%li\n", n);
+	printf("%d\n", n);
 return (0);
 }
