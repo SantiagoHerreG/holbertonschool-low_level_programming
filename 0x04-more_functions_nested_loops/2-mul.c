643785b7594582609f8d158a74a6418833957a8f
SantiagoHerreG <888@holbertonschool.com>
2019-06-27 14:49:36 +0000

    functions similars to the ones created for 0x02 nested loops and functions project

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * mul - computes the product of two numbers
+ * @c: number 1
+ * @a: number 2
+ * Return: the product
+ */
+int mul(int c, int a)
+{
+	int x;
+
+	x = c * a;
+
+	return (x);
+}
