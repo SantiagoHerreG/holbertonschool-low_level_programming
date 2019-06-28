fd720bffb727a4c6be5e275a05daf79784ae602b
SantiagoHerreG <888@holbertonschool.com>
2019-06-28 03:20:06 +0000

    function that prints a number using putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,32 +8,32 @@
 
 void print_number(int n)
 {
-        int i = 1000000000, a = 0;
+	int i = 1000000000, a = 0;
 
-        unsigned int k;
+	unsigned int k;
 
-        if (n < 0)
-        {
-        _putchar(45);
-        k = -n;
-        }
+	if (n < 0)
+	{
+	_putchar(45);
+	k = -n;
+	}
 	else
 	k = n;
-        while (i >= 10)
-        {
-        if (k / i > 0)
-        break;
-        else
-        i = i / 10;
-        }
-        while (i >= 1)
-        {
-        if (k - (i / 10) > 0)
-        {
-        a = k / i;
-        _putchar('0' + a);
-        }
-        else
+	while (i >= 10)
+	{
+	if (k / i > 0)
+	break;
+	else
+	i = i / 10;
+	}
+	while (i >= 1)
+	{
+	if (k - (i / 10) > 0)
+	{
+	a = k / i;
+	_putchar('0' + a);
+	}
+	else
 	{
 	_putchar(48);
 	}
