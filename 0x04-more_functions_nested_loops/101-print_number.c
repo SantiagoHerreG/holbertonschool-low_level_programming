afaa8d78f077281441762d3f57d5c0377ecf3976
SantiagoHerreG <888@holbertonschool.com>
2019-06-28 01:30:11 +0000

    Function that prints a number without using printf, but one character at a time

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,10 +1,45 @@
-void iprint (int n)
-  { 
-    if( n > 9 )
-      { int a = n / 10;
+#include "holberton.h"
 
-        n -= 10 * a;
-        iprint(a);
-      }
-    putchar('0'+ n);
-  }
+/**
+ * print_number - print any integer using putchar
+ * @n: integer number
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	int i = 1000000000, a = 0, negative = -1;
+
+	if (n < 0)
+	{
+	_putchar(45);
+	negative = n % 10;
+	negative = -negative;
+	n = n / 10;
+	n = -n;
+	}
+	while (i >= 10)
+	{
+	if (n / i > 0)
+	break;
+	else
+	i = i / 10;
+	}
+	while (i > 1)
+	{
+	if (n > (i / 10))
+	{
+	a = n / i;
+	_putchar('0' + a);
+	}
+	else
+	{
+	_putchar('0');
+	}
+	n = n % i;
+	i = i / 10;
+	}
+	_putchar('0' + n % 10);
+	if (negative > 0)
+	_putchar('0' + negative);
+}
