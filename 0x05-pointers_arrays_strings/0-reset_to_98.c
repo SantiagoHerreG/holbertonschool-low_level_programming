81336758b69759d400ea551bb02194f0035ca48c
SantiagoHerreG <888@holbertonschool.com>
2019-07-01 20:59:43 +0000

    0-reset: chanching the value of an int from a function using a pointer dereference

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - updates the value of an int from a function
+ * @n: pointer to a variable in the calling function
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
