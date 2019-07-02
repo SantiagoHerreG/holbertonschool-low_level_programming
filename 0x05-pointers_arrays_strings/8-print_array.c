4d35957158227de3b1e3f6d2fd8e8dd3915f381b
SantiagoHerreG <888@holbertonschool.com>
2019-07-02 03:46:24 +0000

    8-print_array: prints n elements of an array of integers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - prints the n elements of an array of integers
+ * @a: array of integers
+ * @n: number of elements for printing
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	printf("%d\n", a[n - 1]);
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,6 +1,7 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 
+void print_array(int *a, int n);
 void puts_half(char *str);
 void puts2(char *str);
 void rev_string(char *s);
