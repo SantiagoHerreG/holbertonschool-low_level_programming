7924ed12c76346ff5fa61d31f1db07109b4b8cfe
SantiagoHerreG <888@holbertonschool.com>
2019-07-03 04:02:02 +0000

    100-atoi: function that converts a string containing numbers to a integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,39 +7,23 @@
  */
 int _atoi(char *s)
 {
-	int i = 0, j = 0, k = 0, m = 0;
+	int i = 0, j = 0, n = 0;
 
-	unsigned int n = 0;
-
-	while (s[m] != '\0')
-	m++;
 	while (s[i] != '\0')
 	{
-	if (s[i] >= '0' && s[i] <= '9')
-	{
-	s[j] = *(s + i);
-	j++;
-	}
-	else if (j > 0)
-	break;
+		if (s[i] == '-')
+		j++;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			n = n * 10 + s[i] - '0';
+		}
+		else if (n > 0)
+		break;
 	i++;
 	}
-	while (j < m)
-	{
-	s[j + 1] = '\0';
-	j++;
-	}
-	if (j == 0)
-	{
+	if (i == 0)
 	return (0);
-	}
-	else
-	{
-	while (s[k] != '\0')
-	{
-	n = (n * 10) + (s[k] - '0');
-	k++;
-	}
-	}
-return (n);
+	if (j % 2 != 0)
+	n = -n;
+	return (n);
 }
