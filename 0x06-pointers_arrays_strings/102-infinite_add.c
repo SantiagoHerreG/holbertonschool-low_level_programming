6bb393ddef19144feeee0e1ff7cc4033c9abe54e
SantiagoHerreG <888@holbertonschool.com>
2019-07-06 02:08:22 +0000

    102-infinitesum: add two infinite numbers with some corrections

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -9,42 +9,44 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int a, b, n = 0;
+	int a, b, n = 0, k = 0;
 
 	for (a = 0; n1[a] != '\0'; a++)
 	;
 	for (b = 0; n2[b] != '\0'; b++)
 	;
-	if (b > (size_r - 1) || a > (size_r - 1))
+	if (b >= (size_r - 1) || a >= (size_r - 1))
 	return (0);
-
 	r[size_r] = '\0';
-	while (a >= 0 && b >= 0)
+	size_r--;
+	while (a > 0 && b > 0)
 	{
-	n = n + (n1[a] - '0') + (n2[b] - '0');
-	r[size_r - 1] = (n % 10 + '0');
+	n = n + (n1[a - 1] - '0') + (n2[b - 1] - '0');
+	r[size_r] = (n % 10 + '0');
+	if (n >= 10)
 	n = n / 10;
 	a--;
 	b--;
 	size_r--;
 	}
-	while (a > 0)
+	while (a > 0 || b > 0)
 	{
-	n = n + (n1[a] - '0');
+	if (a > 0)
+	{
+	n = n + (n1[a - 1] - '0');
 	a--;
-	r[size_r - 1] = (n % 10 + '0');
-	n = n / 10;
-	size_r--;
 	}
-	while (b > 0)
-	{
-	n = n + (n2[b] - '0');
+	else
+	n = n + (n2[b - 1] - '0');
 	b--;
-	r[size_r - 1] = (n % 10 + '0');
+	r[size_r] = (n % 10 + '0');
+	if (n >= 10)
 	n = n / 10;
 	size_r--;
 	}
-	for ( ; size_r - 1 >= 0; size_r--)
-		r[size_r - 1] = '\0';
-	return (r);
+	r[size_r] = (n + '0');
+	for (k = 0; r[k + size_r] != '\0'; k++)
+		r[k] = r[k + size_r];
+		r[k] = '\0';
+return (r);
 }
