99bff278e5675eab13b65cae2964c81210989715
SantiagoHerreG <888@holbertonschool.com>
2019-07-09 03:11:15 +0000

    2-strchr: changes the memory address of a pointer inside an array corr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,15 +9,10 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	if (s[0] == '\0')
-	return ('\0');
-	else
-	{
 	for (i = 0; s[i] != '\0'; i++)
 	{
 	if (s[i] == c)
 	break;
-	}
 	}
 	if (s[i] == '\0')
 	return ('\0');
