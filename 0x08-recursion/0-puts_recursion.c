36496c51c8893c5626ee6e74735e999f82b42646
SantiagoHerreG <888@holbertonschool.com>
2019-07-10 18:27:42 +0000

    0-puts: function that prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,13 +6,10 @@
  */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	if (s[i] != '\0')
+	if (s[0] != '\0')
 	{
-	_putchar(s[i]);
-	i++;
-	_puts_recursion(s + i);
+	_putchar(s[0]);
+	_puts_recursion(s + 1);
 	}
 	else
 	_putchar('\n');
