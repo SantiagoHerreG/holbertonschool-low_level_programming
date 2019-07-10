5badb19a55bcd3692403769e90a678b0678324d9
SantiagoHerreG <888@holbertonschool.com>
2019-07-10 15:31:43 +0000

    1-print_string_rev: prints a string in reverse order

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: pointer to the string
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] != '\0' && s[i + 1] != '\0')
+	{
+	_print_rev_recursion(s + 1);
+	}
+	_putchar(s[i]);
+}
