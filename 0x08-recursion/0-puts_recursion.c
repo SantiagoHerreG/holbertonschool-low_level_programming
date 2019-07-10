00cc6bd1bfdbe8896222c8da036a685636b73f36
SantiagoHerreG <888@holbertonschool.com>
2019-07-10 14:41:44 +0000

    0-puts: function that prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _puts_recursion - prints a string
+ * @s: pointer to the string
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+	_putchar(s[i]);
+	i++;
+	_puts_recursion(s + i);
+	}
+	else
+	_putchar('\n');
+}
