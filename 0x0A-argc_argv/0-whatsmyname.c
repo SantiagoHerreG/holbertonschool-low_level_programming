b3aec8bfa3a3f1121434561d45413ea336bb5237
SantiagoHerreG <888@holbertonschool.com>
2019-07-13 22:50:32 +0000

    0-whatsmyname: program that prints its name (also with the path) using argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints its name
+ * @argc: number of arguments passed to main
+ * @argv: array of pointers to strings passed to main
+ * Return: exit success
+ */
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
