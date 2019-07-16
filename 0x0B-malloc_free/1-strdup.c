8d267511acb480504d4a9ca5d7c21845264cd9a3
SantiagoHerreG <888@holbertonschool.com>
2019-07-16 18:41:29 +0000

    1-strdup: copies a variable length string in the heap using malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to the memory allocating a string
+ * @str: string as a parameter
+ * Return: pointer to the memory allocation of the string
+ */
+
+char *_strdup(char *str)
+{
+	char *a;
+
+	int i, k;
+
+	if (str == NULL)
+		return (NULL);
+
+	i = 0;
+	while (str[i] != 0)
+		i++;
+
+	a = malloc(i);
+
+	k = 0;
+	while (k < i)
+	{
+		a[k] = str[k];
+		k++;
+	}
+	return (a);
+}
