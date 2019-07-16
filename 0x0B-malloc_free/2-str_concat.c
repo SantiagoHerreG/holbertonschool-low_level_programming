950579ad02204133b77b8b0e30a061a55a850e4f
SantiagoHerreG <888@holbertonschool.com>
2019-07-16 20:27:41 +0000

    2-str_concat: concatenates two strings in allocated memory using malloc

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: string one
+ * @s2: second string, to be added to the first one
+ * Return: pointer to the new space in memory
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *a;
+
+	unsigned int i, k, z;
+
+	if (s1 == NULL)
+		s1[0] = 0;
+
+	if (s2 == NULL)
+		s2[0] = 0;
+
+	for (i = 0; s1[i] != 0; i++)
+		;
+	for (k = 0; s2[k] != 0; k++)
+		;
+
+	a = malloc(i + k + 1);
+	if (a == NULL)
+		return (NULL);
+
+	for (z = 0; z < i; z++)
+		a[z] = s1[z];
+
+	for (; z <= i + k - 1; z++)
+	{
+		a[z] = *s2;
+		s2++;
+	}
+	return (a);
+}
