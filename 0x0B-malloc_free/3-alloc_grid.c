fc9febcb964e848af597a1c23c1960a806dfd8b3
SantiagoHerreG <888@holbertonschool.com>
2019-07-16 22:54:16 +0000

    3-alloc_grid: dynamically allocates a two dimensions array in memory and returns a pointer to it

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of int
+ * @width: number of columns
+ * @height: number of rows
+ * Return: pointer to an array of pointers
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **a, i, k;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	a = malloc(sizeof(int *) * height);
+
+	for (i = 0; i < height; i++)
+		a[i] = (int *) malloc(width * sizeof(int));
+
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		for (k = 0; k < width; k++)
+		{
+			a[i][k] = 0;
+		}
+	}
+	return (a);
+}
