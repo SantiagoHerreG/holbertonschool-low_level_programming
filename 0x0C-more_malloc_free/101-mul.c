82edfb6beb4751d3692a3643118a47e216e4f389
SantiagoHerreG <888@holbertonschool.com>
2019-07-19 04:33:03 +0000

    101-mul: Program that multiplies tow numbers as strings, not finished

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
 {
 	char *r, *err = "Error";
 
-	int size_n1, size_n2, n, z, i, i2, f1, f2, lleva = 0, sum = 0, k = 0, m = 0;
+	int size_1, size_2, n, z, i, i2, f1, f2, lleva = 0, sum = 0, k = 0, m = 0;
 
 	if (argc != 3)
 	{
@@ -21,7 +21,6 @@ int main(int argc, char **argv)
 		_putchar('\n');
 		exit(98);
 	}
-
 	for (n = 1; n < argc; n++)
 	{
 	i = 0;
@@ -37,41 +36,38 @@ int main(int argc, char **argv)
 		i++;
 		}
 	}
-
-	for (size_n1 = 0; argv[1][size_n1] != '\0'; size_n1++)
+	for (size_1 = 0; argv[1][size_1] != '\0'; size_1++)
 		;
-
-	for (size_n2 = 0; argv[2][size_n2] != '\0'; size_n2++)
+	for (size_2 = 0; argv[2][size_2] != '\0'; size_2++)
 		;
-
-	if (size_n1 == 0 || size_n2 == 0)
+	if (size_1 == 0 || size_2 == 0)
 	{
 		for (z = 0; err[z] != '\0'; z++)
 			_putchar(err[z]);
 		_putchar('\n');
 		exit(98);
 	}
-	if((size_n1 == 1 && argv[1][0] == '0') || (size_n2 == 1 && argv[2][0] == '0'))
+	if ((size_1 == 1 && argv[1][0] == '0') || (size_2 == 1 && argv[2][0] == '0'))
 	{
 		_putchar('0');
 		_putchar('\n');
-		return(0);
+		return (0);
 	}
-	r = malloc(size_n1 * size_n2 + 1);
+	if (size_1 == 1 && size_2 == 1)
+		r = malloc(size_1 * size_2 + 2);
+	else
+		r = malloc(size_1 * size_2 + 1);
 	if (r == NULL)
 		exit(98);
-
-	for (z = 0; z < size_n1 * size_n2 ; z++)
+	for (z = 0; z < size_1 * size_2 + 1; z++)
 		r[z] = '0';
 	r[z] = '\0';
-
-	for (i = size_n1 - 1; i >= 0; i--)
+	for (i = size_1 - 1; i >= 0; i--)
 	{
 		lleva = 0;
 		f1 = argv[1][i] - '0';
 		k = 0;
-
-		for (i2 = size_n2 - 1; i2 >= 0; i2--)
+		for (i2 = size_2 - 1; i2 >= 0; i2--)
 		{
 			f2 = argv[2][i2] - '0';
 			sum = f1 * f2 + (r[k + m] - '0') + lleva;
@@ -79,22 +75,19 @@ int main(int argc, char **argv)
 			r[k + m] = (sum % 10 + '0');
 			k++;
 		}
-
 		if (lleva > 0)
 			r[k + m] = ((lleva + (r[k + m] - '0')) + '0');
 
 		m++;
 	}
-	z = size_n1 * size_n2;
+	z = size_1 * size_2 + 1;
 	while (z >= 0 && r[z - 1] == '0')
 		z--;
-
 	for ( ; z > 0; z--)
-        {
+	{
 		_putchar(r[z - 1]);
 	}
-	_putchar('\n');	
-
+	_putchar('\n');
 	free(r);
 	return (0);
 }
