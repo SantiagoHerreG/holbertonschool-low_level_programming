566c6d24a38823a3eec3b6058926ba52b3b8a35e
SantiagoHerreG <888@holbertonschool.com>
2019-07-20 01:54:48 +0000

    101-mul: Program that multiplies tow numbers as strings, not finished

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,7 +1,18 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * printerr - function that prints error and exits
+ * @s: Pointer to string Error
+ * Return: void
+ */
+void printerr(char *s)
+{
+	for (; *s != '\0'; s++)
+		_putchar(*s);
+	_putchar('\n');
+	exit(98);
+}
 /**
  * main - progrma that multiplies two big numbers as strings
  * @argc: Number of factors passed as arguments
@@ -12,81 +23,54 @@ int main(int argc, char **argv)
 {
 	char *r, *err = "Error";
 
-	int size_1, size_2, n, z, i, i2, f1, f2, lleva = 0, sum = 0, k = 0, m = 0;
+	int size_1, size_2, n, z = 0, i, i2, f1, f2, lleva = 0, sum = 0, k = 0, m = 0;
 
-	if (argc != 3)
-	{
-		for (i = 0; err[i] != '\0'; i++)
-			_putchar(err[i]);
-		_putchar('\n');
-		exit(98);
-	}
 	for (n = 1; n < argc; n++)
 	{
-	i = 0;
-		while (argv[n][i] != '\0')
+		for (i = 0; argv[n][i] != '\0'; i++)
 		{
-			if (argv[n][i] < '0' || argv[n][i] > '9')
-			{
-				for (z = 0; err[z] != '\0'; z++)
-					_putchar(err[z]);
-				_putchar('\n');
-				exit(98);
-			}
-		i++;
+			if (argv[n][i] < '0' || argv[n][i] > '9' || argc != 3)
+				printerr(err);
+			else if (argv[n][i] != '0')
+				z++;
 		}
+		if (z == 0)
+		{
+			_putchar('0');
+			_putchar('\n');
+			return (0);
+		}
+	z = 0;
 	}
 	for (size_1 = 0; argv[1][size_1] != '\0'; size_1++)
 		;
 	for (size_2 = 0; argv[2][size_2] != '\0'; size_2++)
 		;
-	if (size_1 == 0 || size_2 == 0)
-	{
-		for (z = 0; err[z] != '\0'; z++)
-			_putchar(err[z]);
-		_putchar('\n');
-		exit(98);
-	}
-	if ((size_1 == 1 && argv[1][0] == '0') || (size_2 == 1 && argv[2][0] == '0'))
-	{
-		_putchar('0');
-		_putchar('\n');
-		return (0);
-	}
-	if (size_1 == 1 && size_2 == 1)
-		r = malloc(size_1 * size_2 + 2);
-	else
-		r = malloc(size_1 * size_2 + 1);
+	r = malloc(size_1 * size_2 + 1);
 	if (r == NULL)
 		exit(98);
 	for (z = 0; z < size_1 * size_2 + 1; z++)
 		r[z] = '0';
 	r[z] = '\0';
-	for (i = size_1 - 1; i >= 0; i--)
+	for (i = size_1 - 1; i >= 0; i--, m++)
 	{
 		lleva = 0;
 		f1 = argv[1][i] - '0';
 		k = 0;
-		for (i2 = size_2 - 1; i2 >= 0; i2--)
+		for (i2 = size_2 - 1; i2 >= 0; i2--, k++)
 		{
 			f2 = argv[2][i2] - '0';
 			sum = f1 * f2 + (r[k + m] - '0') + lleva;
 			lleva = sum / 10;
 			r[k + m] = (sum % 10 + '0');
-			k++;
 		}
 		if (lleva > 0)
 			r[k + m] = ((lleva + (r[k + m] - '0')) + '0');
-
-		m++;
 	}
-	z = size_1 * size_2 + 1;
-	while (z >= 0 && r[z - 1] == '0')
-		z--;
+	for (z = size_1 * size_2 + 1; z >= 0 && r[z - 1] == '0'; z--)
+		;
 	for ( ; z > 0; z--)
-	{
 		_putchar(r[z - 1]);
-	}
 	_putchar('\n');
 	free(r);
 	return (0);
--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -7,6 +7,6 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
-void _puts(char *str);
+void printerr(char *s);
 
 #endif
