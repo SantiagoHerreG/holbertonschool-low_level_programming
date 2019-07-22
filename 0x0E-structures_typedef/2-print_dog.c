a8c07c57b1e4fa7980355dcb46908673865c4f40
SantiagoHerreG <888@holbertonschool.com>
2019-07-22 21:24:12 +0000

    2-print_dog: Function that prints the info in a structure variable

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_dog - function that prints a structure
+ * @d: pointer to the structure
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name)
+		printf("Name: %s\n", d->name);
+	else
+		printf("Name: (nil)");
+	if (d->age)
+		printf("Age: %f\n", d->age);
+	else
+		printf("Age: (nil)");
+	if (d->owner)
+		printf("Owner: %s\n", d->owner);
+	else
+		printf("Owner: (nil)");
+}
