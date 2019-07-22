8358648c198a85ee2b6f0b6890a69e237f146679
SantiagoHerreG <888@holbertonschool.com>
2019-07-22 22:46:00 +0000

    4-new_dog: Function that creates and stores a new variable of type struct dog (dog_t)

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - Function that creates and stores a new variable type dog
+ * @name: name of the dog
+ * @age: of the dog
+ * @owner: of the dog
+ * Return: Pointer to a structure
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_d;
+
+	new_d = malloc(sizeof(dog_t));
+
+	if (new_d == NULL)
+		return (NULL);
+
+	new_d->name = name;
+	new_d->age = age;
+	new_d->owner = owner;
+
+	return (new_d);
+}
