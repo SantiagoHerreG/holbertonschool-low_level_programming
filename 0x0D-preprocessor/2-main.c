b455dbf87b2c03589a7baf83d361f1a4a7092f9b
SantiagoHerreG <888@holbertonschool.com>
2019-07-22 17:58:01 +0000

    2-main.c: Program that prints ist own name using macro __FILE__, correction with putchar.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
-
+#include "holberton.h"
 /**
  * main - program that prints in std output its name
  *
  * Return: void
  */
 
-int main()
+int main(void)
 {
+	char *p = __FILE__;
 
-	printf("%s\n", __FILE__);
+	while (*p != '\0')
+	{
+	_putchar(*p);
+	p++;
+	}
+	_putchar('\n');
 	return (0);
 }
