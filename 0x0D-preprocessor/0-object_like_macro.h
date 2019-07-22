88724266be4f46dc9198deeeb029685bef4b4662
SantiagoHerreG <888@holbertonschool.com>
2019-07-22 16:30:15 +0000

    0-object_like_macro: header file that contains a macro named size for 1024

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef __0_OBJECT_LIKE_MACRO__H__
+#define __0_OBJECT_LIKE_MACRO__H__
+
+#define SIZE 1024
+
+#endif /* #ifndef __0_OBJECT_LIKE_MACRO__H__ */
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+# Project intended to learn how to use preprocessor directives and macros#
