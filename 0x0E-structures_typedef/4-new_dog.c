f1fe696db01b47d4e1786ff09de8503303fd566e
SantiagoHerreG <888@holbertonschool.com>
2019-07-23 03:26:03 +0000

    4-new_dog: Function that creates and stores a new variable of type struct dog, correction of copies in memory

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,47 +8,41 @@
  * @owner: of the dog
  * Return: Pointer to a structure
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_d;
 
 	int i;
 
-	char *cpyname, *cpyowner;
-
 	new_d = malloc(sizeof(dog_t));
 	if (new_d == NULL)
 		return (NULL);
 
-	cpyname = malloc(sizeof(char *));
-
-	if (cpyname == NULL)
-	{
-		free(new_d);
-		return (NULL);
-	}
-
 	for (i = 0; name[i] != 0; i++)
-		cpyname[i] = name[i];
-	cpyname[i] = name[i];
-
-	cpyowner = malloc(sizeof(char *));
-
-	if (cpyowner == NULL)
+		;
+	new_d->name = malloc(i * sizeof(char));
+	if (new_d->name == NULL)
 	{
 		free(new_d);
-		free(cpyname);
 		return (NULL);
 	}
+	for (i = 0; name[i] != 0; i++)
+		new_d->name[i] = name[i];
+	new_d->name[i] = name[i];
 
 	for (i = 0; owner[i] != 0; i++)
-		cpyowner[i] = owner[i];
-	cpyowner[i] = owner[i];
+		;
+	new_d->owner = malloc(i * sizeof(char));
+	if (new_d->owner == NULL)
+	{
+		free(new_d);
+		free(new_d->name);
+		return (NULL);
+	}
+	for (i = 0; owner[i] != 0; i++)
+		new_d->owner[i] = owner[i];
+	new_d->owner[i] = owner[i];
 
-	new_d->name = cpyname;
 	new_d->age = age;
-	new_d->owner = cpyowner;
-
 	return (new_d);
 }
