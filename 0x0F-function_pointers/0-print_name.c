34f25f1da5ae6e0fe490db23e964c0865eb2df6c
SantiagoHerreG <888@holbertonschool.com>
2019-07-24 15:31:54 +0000

    0-print_name: Function that performs a call back to print a string

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name using function pointers
+ * @name: string to be printed
+ * @f: function address
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+# Project intended to learn function pointers and how to use them
