d3ccb0fbed0a1fa8197eb9db0fe87f7120af22d1
SantiagoHerreG <888@holbertonschool.com>
2019-07-25 03:14:36 +0000

    100-main_opcodes: program that prinst its own opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - program that prints its own function
  * @argc: number of arguments passed
  * @argv: pointer to array of strings
  * Return: 0 on succes
  */
-
 int main(int argc, char **argv)
 {
 	int a, b;
@@ -28,18 +26,25 @@ int main(int argc, char **argv)
 		exit(2);
 	}
 
-	unsigned int *function_A = (unsigned int *) main;
+	unsigned int *ptr_main = (unsigned int *) main;
 
 	for (i = 0; (i * 4) < a; i++)
 	{
-		z = (*(function_A + i));
+		z = (*(ptr_main + i));
 		for (b = 0; b < 4 && (4 * i) + b < a - 1; b++)
 		{
-			printf("%x ", z % 256);
+			if (z % 256 != 0)
+				printf("%02x ", z % 256);
 			z = z / 256;
+			if (z % 256 == 0 && b < 3)
+			{
+				a++;
+				b++;
+			}
 		}
-		if ((4 * i) + b + 1 == a)
-			printf("%x\n", z % 256);
-		b = 0;
+		if ((4 * i) + b == a - 1 && z % 256 != 0)
+			printf("%02x\n", z % 256);
+		else if (b < 4)
+			a = a + 4;
 	}
 }
