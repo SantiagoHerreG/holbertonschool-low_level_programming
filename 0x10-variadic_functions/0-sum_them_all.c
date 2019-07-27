4bf1b6514536acd9507d379b0a13b03d0e51cd96
SantiagoHerreG <888@holbertonschool.com>
2019-07-27 18:36:05 +0000

    0-sum_them_all: variadic function that returns the sum of n numbers

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,16 +10,22 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	unsigned int i;
+
+	int sum = 0;
+
 	if (n == 0)
 		return (0);
 
-	va_list ap;
-	unsigned int sum = 0, i;
+	if (n == '\0')
+		return (1);
 
+	va_list ap;
+	
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(ap, const unsigned int);
+		sum += va_arg(ap, int);
 
 	va_end(ap);
 	return (sum);
