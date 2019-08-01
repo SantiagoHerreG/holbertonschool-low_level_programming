69093507f8a7bd710ec8099d1c6e06fb2a085f85
SantiagoHerreG <888@holbertonschool.com>
2019-08-01 22:38:44 +0000

    100-first: function that prints a phrase before main is executed

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -19,5 +19,7 @@ unsigned int list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void print_before(void) __attribute__ ((constructor));
+
 
 #endif /* __LISTS_H__ */
