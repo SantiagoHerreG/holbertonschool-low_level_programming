76ac3ae84ce5fea55d0e86a44bd4b3f612b874ad
SantiagoHerreG <888@holbertonschool.com>
2019-08-01 18:44:47 +0000

    2-add_node: function that adds a new node at the beginning of a list_t list (linked list)

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -16,5 +16,6 @@ typedef struct list_s
 
 unsigned int print_list(const list_t *h);
 unsigned int list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif /* __LISTS_H__ */
