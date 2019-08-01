11704a39423eb03c0836b63c1bc83d4f6abc0e11
SantiagoHerreG <888@holbertonschool.com>
2019-08-01 19:28:10 +0000

    3-add_node_end: function that adds a new node at the end of a list_t list (linked list)

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -17,5 +17,7 @@ typedef struct list_s
 unsigned int print_list(const list_t *h);
 unsigned int list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+
 
 #endif /* __LISTS_H__ */
