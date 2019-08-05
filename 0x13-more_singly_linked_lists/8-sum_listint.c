33fe1c926df0181b7802cb262e6d5b79f358ddd5
SantiagoHerreG <888@holbertonschool.com>
2019-08-05 16:23:20 +0000

    8-sum_listint: function that adds the integer data in a list until the end

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * sum_listint - function that adds every single node data
+ * @head: pointer to the beginning of the list
+ * Return: returns the cummulative sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum;
+
+	listint_t *temp;
+
+	if (!head)
+		return (0);
+
+	temp = head;
+
+	while (temp)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (sum);
+}
