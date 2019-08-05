ce36d84a18c706e4dfacd78076a86cdb8128751d
SantiagoHerreG <888@holbertonschool.com>
2019-08-05 19:56:45 +0000

    101-print_listint: function that can print a list with a loop

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_listint_safe - function that can print a list with loops
+ * @head: pointer to the first node
+ * Return: count of elements
+ */
+unsigned int print_listint_safe(const listint_t *head)
+{
+	listint_t *array[10000], *last;
+
+	unsigned int i = 0, k = 0;
+
+	if (!head)
+		exit(98);
+
+	last = (listint_t *)head;
+
+	while (last)
+	{
+		for (k = 0; k < i; k++)
+		{
+			if (last == array[k])
+			{
+				printf("-> [%p] %d\n", (void *)last, last->n);
+				return (i);
+			}
+		}
+		printf("[%p] %d\n", (void *)last, last->n);
+		array[i] = last;
+		i++;
+		last = last->next;
+	}
+	return (i);
+}
