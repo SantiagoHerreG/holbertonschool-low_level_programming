ae118ee07ecfaa069d2d3127bbbae48ae0762f4d
SantiagoHerreG <888@holbertonschool.com>
2019-08-05 16:22:17 +0000

    7-get_nodeint: returns a pointer to a node in nth position

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * get_nodeint_at_index - function that returns a pointer to a node in a list
+ * @head: pointer to the first element
+ * @index: position of the element
+ * Return: pointer to the element
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *ptr_temp;
+
+	unsigned int i = 0;
+
+	if (!head)
+		return (NULL);
+
+	ptr_temp = head;
+
+	if (!ptr_temp->next && index == 0)
+		return (ptr_temp);
+
+	while (ptr_temp->next)
+	{
+		ptr_temp = ptr_temp->next;
+		i++;
+		if (i == index)
+			return (ptr_temp);
+	}
+	return (NULL);
+}
