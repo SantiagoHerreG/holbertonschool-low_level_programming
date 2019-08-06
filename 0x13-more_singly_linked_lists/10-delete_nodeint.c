ef260ac37b98475299515989daa2d9e931bdd724
SantiagoHerreG <888@holbertonschool.com>
2019-08-06 16:26:02 +0000

    10-delete_nodeint: function that deletes a node in the nth place, correction of returning

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp, *next_node, *node_to_free;
 
 	if (!(*head))
-		return (1);
+		return (-1);
 
 	temp = *head;
 	if (index == 0)
