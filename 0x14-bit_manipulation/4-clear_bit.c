0564e25a311447fcc0fe3b821d965b872aa82d24
SantiagoHerreG <888@holbertonschool.com>
2019-08-08 20:28:44 +0000

    4-clear_bit: function that clears a bit in a number (sets to 0)

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * clear_bit - function that clears the value of a bit (to 0) at an index place
+ * @n: pointer to the address of a number to be set
+ * @index: place in the number
+ * Return: 1 if it worked or -1 else
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int bit = 1;
+
+	bit = bit << index;
+
+	*n &= ~bit;
+		return (1);
+}
