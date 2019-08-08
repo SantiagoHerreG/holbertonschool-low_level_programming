0ef3716a7f3039d87a5efc336f8a7645e67c8756
SantiagoHerreG <888@holbertonschool.com>
2019-08-08 20:05:17 +0000

    3-set_bit: function that sets a bit of a number into 1 if it is 0

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -16,13 +16,14 @@ int get_bit(unsigned long int n, unsigned int index)
 		return (0);
 	else if (n == 1 && index == 0)
 		return (1);
-	else if (n > 1)
+	else
 	{
 		if (bit > n)
 			return (-1);
 
-		return (bit & n ? 1 : 0);
+		if (bit & n)
+			return (1);
+		else
+			return (0);
 	}
-	else
-		return (-1);
 }
---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * set_bit - function that sets the value of a bit to 1 at an index place
+ * @n: pointer to the address of a number to be set
+ * @index: place in the number
+ * Return: 1 if it worked or -1 else
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int bit = 1;
+
+	bit = bit << index;
+
+	if (~(*n) & bit)
+	{
+		*n += bit;
+		return (1);
+	}
+	else
+		return (-1);
+}
