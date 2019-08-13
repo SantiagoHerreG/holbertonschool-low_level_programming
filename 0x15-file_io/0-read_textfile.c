091e496181298d1be4dd8ece809da5f6f424c7ed
SantiagoHerreG <888@holbertonschool.com>
2019-08-13 18:49:22 +0000

    =-read_textfile: function that reads a file and prints their content in stdout

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - function that reads from a file and prints to std output
+ * @filename: name of the file to be read
+ * @letters: number of chars to be read and printed
+ * Return: the number of chars printed or 0 if error
+ */
+size_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	size_t read_chars, write_chars;
+
+	char buffer[letters];
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd < 0)
+		return (0);
+
+	read_chars = read(fd, buffer, letters);
+
+	close(fd);
+
+	if (read_chars < 1)
+		return (0);
+
+	write_chars = write(STDOUT_FILENO, buffer, read_chars);
+
+	if (write_chars != read_chars)
+		return (0);
+
+	return (write_chars);
+}
