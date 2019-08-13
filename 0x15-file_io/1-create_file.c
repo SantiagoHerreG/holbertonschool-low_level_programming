0c85b6fdbec22abe5354f0dc527e8f031cc3538b
SantiagoHerreG <888@holbertonschool.com>
2019-08-13 21:08:56 +0000

    1-create_file: function that creates a file and truncates a string into that, return correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,15 +26,13 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content)
+	{
 		for (len = 0; text_content[len]; len++)
 			;
-	else
-		len = 0;
+		write_chars = write(fd, text_content, len);
 
-	write_chars = write(fd, text_content, len);
-
-	if (write_chars < 0)
-		return (-1);
-
+		if (write_chars < 0)
+			return (-1);
+	}
 	return (1);
 }
