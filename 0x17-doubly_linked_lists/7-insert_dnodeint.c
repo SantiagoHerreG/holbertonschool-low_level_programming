1067286eb3996e943b7567b75d29c500319f5d1b
SantiagoHerreG <888@holbertonschool.com>
2019-09-14 20:56:25 +0000

    7: function that inserts a new node in a certain index of adouble linked list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,8 +19,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = NULL;
 	new_node->prev = NULL;
-
-
 	if (!h)
 		return (NULL);
 
@@ -29,8 +27,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = new_node;
 		return (new_node);
 	}
+	else if (idx == 0)
+	{
+		new_node->next = *h;
+		(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
 	temp = *h;
-
 	while (temp || idx == node_ind)
 	{
 		if (idx == node_ind)
