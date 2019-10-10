051fa1385134445b74d32a42a05cbf2d150b21ad
SantiagoHerreG <888@holbertonschool.com>
2019-10-10 21:32:35 +0000

    0-create hash table:  modification: the new allocation was performed using calloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,12 +13,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (size == 0)
 		return (NULL);
 
-	new_table = malloc(sizeof(hash_table_t));
+	new_table = calloc(1, sizeof(hash_table_t));
 	if (!new_table)
 		return (NULL);
 
 	new_table->size = size;
-	new_table->array = malloc(sizeof(char *) * size);
+	new_table->array = calloc(size, sizeof(char *));
 
 	if (!new_table->array)
 		return (NULL);
