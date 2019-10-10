2d587988e88252134f4da4f9f47985d0adf44087
SantiagoHerreG <888@holbertonschool.com>
2019-10-10 22:25:25 +0000

    3-hash table setting function, with linked list as a collision resolver

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,43 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function that adds an element to the hash table.
+ * @ht: pointer to the hash table structure
+ * @key: string to be looked for
+ * @value: value of the data
+ * Return: 1 on success or 0 on error
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node;
+
+	if (!key || !*key || !value)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	new_node = calloc(1, sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+	new_node->key = calloc(strlen(key) + 1, 1);
+	if (!new_node->key)
+		return (0);
+	new_node->value = calloc(strlen(value) + 1, 1);
+	if (!new_node->value)
+		return (0);
+
+	strcpy(new_node->key, key);
+	strcpy(new_node->value, value);
+	new_node->next = NULL;
+
+	if (ht->array[index] == NULL)
+		ht->array[index] = new_node;
+	else
+	{
+		new_node->next = ht->array[index];
+		ht->array[index] = new_node;
+	}
+	return (1);
+}
