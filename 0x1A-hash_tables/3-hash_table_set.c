de1fded6bd29d111492a6fb16b4b43f44e19d1d3
SantiagoHerreG <888@holbertonschool.com>
2019-10-11 13:16:24 +0000

    3-hash table setter: structure modified to check and overwrite keys before creating a new node, correction of edge case null ht

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node, *temp;
 
-	if (!key || !*key || !value)
+	if (!ht || !key || !*key || !value)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 
