5f7389be9ca404bfa75eb209e5f318075b61cc9d
SantiagoHerreG <888@holbertonschool.com>
2019-10-11 03:52:40 +0000

    3-hash table setter: structure modified to check and overwrite keys before creating a new node

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,6 +17,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 
+	temp = ht->array[index];
+	while (temp)
+	{
+		if (!strcmp(temp->key, key))
+		{
+			free(temp->value);
+			temp->value = calloc(strlen(value) + 1, sizeof(char));
+			if (!temp->value)
+				return (0);
+			strcpy(temp->value, value);
+			return (1);
+		}
+		temp = temp->next;
+	}
 	new_node = calloc(1, sizeof(hash_node_t));
 	if (!new_node)
 		return (0);
@@ -26,27 +40,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->value = calloc(strlen(value) + 1, 1);
 	if (!new_node->value)
 		return (0);
-
 	strcpy(new_node->key, key), strcpy(new_node->value, value);
 	new_node->next = NULL;
-	if (ht->array[index] == NULL)
+
+	if (!ht->array[index])
 		ht->array[index] = new_node;
 	else
 	{
-		temp = ht->array[index];
-		while (temp)
-		{
-			if (!strcmp(temp->key, key))
-			{
-				free(temp->value);
-				temp->value = calloc(strlen(value) + 1, 1);
-				if (!temp->value)
-					return (0);
-				strcpy(temp->value, value);
-				return (1);
-			}
-			temp = temp->next;
-		}
 		new_node->next = ht->array[index], ht->array[index] = new_node;
 	}
 	return (1);
