f5bd74463d5884bc1133986472b82a138fe298ad
SantiagoHerreG <888@holbertonschool.com>
2019-10-30 04:16:13 +0000

    104-heap sort: algorithm that sorts a list of integers by using bunary heap in arrays -binary trees-

--- 0x1B-sorting_algorithms/104-heap_sort.c
+++ 0x1B-sorting_algorithms/104-heap_sort.c
@@ -1,21 +1,46 @@
 #include "sort.h"
-#include <stdio.h>
+/**
+ * heap_tree - sorts using the heap sorting algorithm
+ *
+ * @array: array of integers to be sorted
+ * @size: size of the array
+ * @end: positional variable
+ * Return: void
+ */
 void heap_tree(int *array, size_t size, size_t end)
 {
-	size_t parent, end_copy;
+	size_t parent, end_copy, flag = 0;
 
 	end_copy = end;
 	parent = (end - 1) / 2;
 	while (end)
-	{ 
-		if (array[end] > array[parent])
+	{
+		if (end % 2 == 1 && end != end_copy && (array[end]
+		> array[parent] || array[end + 1] > array[parent]))
+		{
+			if (array[end] < array[end + 1])
+				end++;
+			flag++;
+		}
+		else if (end % 2 == 0 && (array[end] > array[parent]
+		|| array[end - 1] > array[parent]))
+		{
+			if (array[end] < array[end - 1])
+				end--;
+			flag++;
+		}
+		else if (end % 2 == 1 && end == end_copy &&
+		array[end] > array[parent])
+			flag++;
+		if (flag)
 		{
 			array[parent] += array[end];
 			array[end] = array[parent] - array[end];
-			array[parent] -=array[end];
+			array[parent] -= array[end];
 			print_array(array, size);
 			end = end_copy;
 			parent = (end - 1) / 2;
+			flag = 0;
 		}
 		else
 		{
@@ -33,5 +58,19 @@ void heap_tree(int *array, size_t size, size_t end)
  */
 void heap_sort(int *array, size_t size)
 {
-	heap_tree(array, size, size);
+	size_t end;
+
+	if (!array || size <= 1)
+		return;
+
+	end = size - 1;
+	while (end)
+	{
+		heap_tree(array, size, end);
+		array[0] += array[end];
+		array[end] = array[0] - array[end];
+		array[0] -= array[end];
+		print_array(array, size);
+		end--;
+	}
 }
