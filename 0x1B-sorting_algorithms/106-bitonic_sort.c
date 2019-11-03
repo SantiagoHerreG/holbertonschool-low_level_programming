89a22cee1cd756046d686df635f72e3536c83800
SantiagoHerreG <888@holbertonschool.com>
2019-11-03 23:51:47 +0000

    106-bitonic_sort: algorithm that uses recursion and sucesive comparison to sort an array of numbers-- bitonic method

--- 0x1B-sorting_algorithms/106-bitonic_sort.c
+++ 0x1B-sorting_algorithms/106-bitonic_sort.c
@@ -1,56 +1,57 @@
 #include "sort.h"
 #include <stdio.h>
-/*
- * @bit: zero is up and 1 is down
+/**
+ * bitonic_merge - sorts an array of 2^k numbers by using bitonic recursion
+ * @array: array of numbers
+ * @lo: lower index of the partial range
+ * @hi: higher index of the partial range
+ * @size: size of the entire array
+ * @bit: 0 is up and 1 is down
  */
-void bitonic_merge(int *array, size_t lo, size_t hi, size_t count, size_t bit, size_t size)
+void bitonic_merge(int *array, size_t lo, size_t hi, size_t bit, size_t size)
 {
-	size_t split;
+	size_t split, i, k;
 
 	if (bit == 0)
 		printf("Merging [%lu/%lu] (UP):\n", hi - lo + 1, size);
 	else
 		printf("Merging [%lu/%lu] (DOWN):\n", hi - lo + 1, size);
-	
 	print_array(array + lo, hi - lo + 1);
-
 	split = (hi - lo) / 2 + 1 + lo;
 	if (hi - lo + 1 > 2)
 	{
-		bitonic_merge(array, lo, split - 1, count, 0, size);
-		bitonic_merge(array, split, hi, count, 1, size);
+		bitonic_merge(array, lo, split - 1, 0, size);
+		bitonic_merge(array, split, hi, 1, size);
 	}
-	if (hi - lo + 1 == 2)
+	i = (hi - lo) / 2 + 1;
+	while (i)
 	{
-		if (bit == 0)
-		{
-			if (array[lo] > array[hi])
+		k = 0;
+		for ( ; bit == 0 && lo + i + k <= hi ; k++)
+			if (array[lo + k] > array[lo + i + k])
 			{
-				array[lo] += array[hi];
-				array[hi] = array[lo] - array[hi];
-				array[lo] -= array[hi];
+				array[lo + k] += array[lo + i + k];
+				array[lo + i + k] = array[lo + k] - array[lo + i + k];
+				array[lo + k] -= array[lo + i + k];
 			}
-		}
-		else
-		{
-			if (array[lo] < array[hi])
+		for ( ; bit == 1 && lo + i + k <= hi ; k++)
+			if (array[lo + k] < array[lo + i + k])
 			{
-				array[lo] += array[hi];
-				array[hi] = array[lo] - array[hi];
-				array[lo] -= array[hi];
+				array[lo + k] += array[lo + i + k];
+				array[lo + i + k] = array[lo + k] - array[lo + i +  k];
+				array[lo + k] -= array[lo + i + k];
 			}
-		}
+		i /= 2;
 	}
 	if (bit == 0)
 		printf("Result [%lu/%lu] (UP):\n", hi - lo + 1, size);
 	else
 		printf("Result [%lu/%lu] (DOWN):\n", hi - lo + 1, size);
-		
 	print_array(array + lo, hi - lo + 1);
 }
 
 /**
- * bitonic_sort - sorts the radix algorithm
+ * bitonic_sort - sorts by using th bitonic algorithm
  *
  * @array: array of integers to be sorted
  * @size: size of the array
@@ -58,14 +59,9 @@ void bitonic_merge(int *array, size_t lo, size_t hi, size_t count, size_t bit, s
  */
 void bitonic_sort(int *array, size_t size)
 {
-	size_t k = 0, count = 0, bit = 0, lo, hi;
+	size_t bit = 0, lo, hi;
 
-	for (k = size; k > 1;)
-	{
-		k /= 2;
-		count++;
-	}
 	lo = 0;
 	hi = size - 1;
-	bitonic_merge(array, lo, hi, count, bit, size);
+	bitonic_merge(array, lo, hi, bit, size);
 }
