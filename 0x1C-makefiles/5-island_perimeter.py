e872bcd74d8df1c1e230c1f23120d8e7083ad184
SantiagoHerreG <888@holbertonschool.com>
2019-11-16 01:20:47 +0000

    5-island perimeter, function that returns the perimeter of a matrix that mocks an island fixed vertical check

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,16 +4,10 @@ Function that returns the perimeter of a matrix that represents an island
 """
 
 
-def one_direction_check(grid):
+def one_direction_hor(grid, h, a):
     """ Checks for the perimeter in horizontal direction
     """
     count = 0
-    h = len(grid)
-    if h == 0:
-        return 0
-    a = len(grid[0])
-    if a == 0:
-        return 0
 
     for i in range(h):
         for j in range(a):
@@ -28,12 +22,36 @@ def one_direction_check(grid):
     return count
 
 
+def one_direction_ver(grid, h, a):
+    """ Returns the oerimeter in vertical direction
+    """
+
+    count = 0
+
+    for i in range(h):
+        for j in range(a):
+            if i == 0 or i + 1 == h:
+                if grid[i][j] == 1:
+                    count += 1
+            if grid[i][j] == 1:
+                if grid[i + 1][j] == 0:
+                    count += 1
+                if grid[i - 1][j] == 0:
+                    count += 1
+    return count
+
+
 def island_perimeter(grid):
     """ Returns an integer that represents the perimeter of an island
     """
-    count_hor = one_direction_check(grid)
-    transv_grid = [[grid[col][rows[col]] for rows in grid] for col in
-                   range(len(grid))]
-    count_ver = one_direction_check(grid)
+    h = len(grid)
+    if h == 0:
+        return 0
+    a = len(grid[0])
+    if a == 0:
+        return 0
+    count_hor = one_direction_hor(grid, h, a)
+
+    count_ver = one_direction_ver(grid, h, a)
 
     return count_hor + count_ver
