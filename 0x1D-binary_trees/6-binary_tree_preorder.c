f2465c5fa32f6abde75ec69b4747ff3bad301a2e
SantiagoHerreG <888@holbertonschool.com>
2019-12-04 18:51:18 +0000

    new corrections of betty and new tasks added

--- 0x1D-binary_trees/6-binary_tree_preorder.c
+++ 0x1D-binary_trees/6-binary_tree_preorder.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - Insert a new node in the tree.
+ * binary_tree_preorder - Insert a new node in the tree.
  *
- * @parent: Parent node.
- * @value: Value of the node.
+ * @tree: Root of the tree.
+ * @func: function to excecute.
  *
  * Return: Always 0 (Success)
  */
@@ -18,7 +18,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 
 	tmp = tree;
 	if (tmp)
-	{	
+	{
 		func(tmp->n);
 		if (tmp->left)
 			binary_tree_preorder(tmp->left, func);
--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -14,10 +14,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -58,4 +58,5 @@ int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const
 binary_tree_t *second);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 #endif
