60554c982bf0fccb2dca58500ae3ec894dcd5072
SantiagoHerreG <888@holbertonschool.com>
2020-01-14 14:26:54 +0000

    search_algos.h: header file for the project

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,6 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 int linear_search(int *array, size_t size, int value);
-
+int binary_search(int *array, size_t size, int value);
 
 #endif /* __SEARCH_ALGOS_H__ */
