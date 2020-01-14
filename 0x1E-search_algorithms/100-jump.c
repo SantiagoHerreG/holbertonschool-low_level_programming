21acf49665eeb24259f25a2abd0b595e5e40bda3
SantiagoHerreG <888@holbertonschool.com>
2020-01-14 16:41:36 +0000

    100-jump: returns an integer by performing jump search

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,36 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_search - searches using the Jump search algorithm
+ *
+ * @array: array of integers to be traversed
+ * @size: size of the array
+ * @value: value to be searched for
+ * Return: integer, index found
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t i, j, m;
+
+	m = (size_t)floor(sqrt(size));
+
+	i = 0;
+	while (i * m < size)
+	{
+		if (array[i * m] >= value)
+			break;
+		printf("Value checked array[%li] = [%d]\n", i * m, array[i * m]);
+		i++;
+	}
+
+	printf("Value found between indexes [%li] and [%li]\n", (i - 1) * m, i * m);
+
+	for (j = (i - 1) * m; j <= i * m && j < size; j++)
+	{
+		printf("Value checked array[%li] = [%d]\n", j, array[j]);
+		if (array[j] == value)
+			return (j);
+	}
+	return (-1);
+}
