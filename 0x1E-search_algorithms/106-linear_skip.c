64ac7bbd199253410428f2e72673c424384eb5e0
SantiagoHerreG <888@holbertonschool.com>
2020-01-15 16:21:00 +0000

    106-linear_skip: uses the skip list algorithm for searching, using the square root of list size as lane step

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,48 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * linear_skip - searches using the linear skip list search algorithm
+ *
+ * @list: linked list of integers to be traversed
+ * @value: value to be searched for
+ * Return: integer, index found
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *temp1, *temp2;
+
+	if (!list)
+		return (NULL);
+	temp1 = list;
+
+	while (temp1->express)
+	{
+		printf("Value checked at index [%li] = [%d]\n",
+		temp1->express->index, temp1->express->n);
+		if (temp1->express->n > value)
+			break;
+		temp1 = temp1->express;
+	}
+	if (!temp1->express)
+	{
+		temp2 = temp1;
+		while (temp2->next)
+			temp2 = temp2->next;
+	}
+	else
+	{
+		temp2 = temp1->express;
+	}
+
+	printf("Value found between indexes [%li] and [%li]\n",
+	temp1->index, temp2->index);
+	while (temp1 != temp2->next)
+	{
+		printf("Value checked at index [%li] = [%d]\n", temp1->index, temp1->n);
+		if (temp1->n == value)
+			return (temp1);
+		temp1 = temp1->next;
+	}
+	return (NULL);
+}
