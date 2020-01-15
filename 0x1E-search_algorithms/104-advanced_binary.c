760d508e61896d333bbc90bbc6288f493d88dc59
SantiagoHerreG <888@holbertonschool.com>
2020-01-15 01:32:52 +0000

    104-advanced_binary: improves the binary search to return the lower index matching

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,54 @@
+#include "search_algos.h"
+
+/**
+ * binary_recursion - uses the recursion to performe the binary search
+ * @array: array of integers to be traversed
+ * @li: the lowest index
+ * @hi: highest index
+ * @value: value to be searched for
+ * Return: integer, index found
+ */
+int binary_recursion(int *array, size_t li, size_t hi, int value)
+{
+	size_t i;
+
+	printf("Searching in array: %d", array[li]);
+
+	for (i = li + 1; i <= hi; i++)
+		printf(", %d", array[i]);
+
+	printf("\n");
+
+	i = (hi - li) / 2 + li;
+
+	if (array[i] == value)
+	{
+		if (i == 0)
+			return (i);
+		else if (array[i - 1] != value)
+			return (i);
+		else
+			return (binary_recursion(array, i - 1, i, value));
+	}
+	else if (hi - li == 0)
+		return (-1);
+	else if (array[i] > value)
+		return (binary_recursion(array, li, i - 1, value));
+	else
+		return (binary_recursion(array, i + 1, hi, value));
+}
+
+/**
+ * advanced_binary - traverse an array and returns the searched value and index
+ * @array: array of integers to be traversed
+ * @size: size of the array
+ * @value: value to be searched for
+ * Return: integer, index found
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	if (!array)
+		return (-1);
+
+	return (binary_recursion(array, 0, size - 1, value));
+}
