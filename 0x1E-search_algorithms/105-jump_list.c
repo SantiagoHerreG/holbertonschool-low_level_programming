8b8fc905967e9348bd30d8b57e60f75859c44fbf
SantiagoHerreG <888@holbertonschool.com>
2020-01-16 15:23:26 +0000

    105-jump: uses the jump algorithm for finding a value in a linked list, optimized algo, when a number is not in the list returns faster

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -20,35 +20,32 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 	m = (size_t)sqrt(size);
 	i = m;
 	temp = list;
+	current = list;
 	while (i < size)
 	{
 		for (j = 0; j < m; j++)
 			temp = temp->next;
 		printf("Value checked at index[%li] = [%d]\n", temp->index, temp->n);
-		if (temp->n > value)
+		if (temp->n >= value)
 			break;
+		current = temp;
 		i += m;
 	}
 	if (i >= size)
 	{
-		current = temp;
-		while (current->next)
-			current = current->next;
-	}
-	else
-	{
-		current = temp, temp = list;
-		for (j = 0; j < i - m; j++)
+		while (temp->next)
 			temp = temp->next;
 	}
-	printf("Value found between indexes [%li] and [%li]\n", temp->index,
-	current->index);
-	while (current->next != temp)
+	printf("Value found between indexes [%li] and [%li]\n",
+	current->index, temp->index);
+	while (current != temp->next)
 	{
-		printf("Value checked array[%li] = [%d]\n", temp->index, temp->n);
-		if (temp->n == value)
-			return (temp);
-		temp = temp->next;
+		printf("Value checked array[%li] = [%d]\n", current->index, current->n);
+		if (current->n == value)
+			return (current);
+		else if (current->n > value)
+			return (NULL);
+		current = current->next;
 	}
 	return (NULL);
 }
